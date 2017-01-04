0a5aea87b4c93226c615aac693de6c5abfd1085d
https://github.com/jed1978/TDD-Kata-TennisScorer.git <jed1978@gmail.com>
2017-01-05 01:26:33 +0800

    Init version with 2 Test Cases

---
+++ TennisGame.Test/GameTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGame.Test
+{
+    [TestClass]
+    public class GameTest
+    {
+        private Game target;
+
+        [TestInitialize]
+        public void Init()
+        {
+            target = new Game("PlayerOne", "PlayerTwo");
+        }
+
+        [TestMethod]
+        public void TestOpenGame()
+        {
+            Assert.AreEqual("Love All", target.Score());
+        }
+
+        [TestMethod]
+        public void TestFifteenLove()
+        {
+            target.PlayerOneWinPoint();
+            Assert.AreEqual("Fifteen Love", target.Score());
+        }
+
+
+    }
+}
---
+++ TennisGame/Game.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TennisGame
+{
+    public class Game
+    {
+        private string _server;
+        private string _receiver;
+        private int _playerOneScore;
+        private int _playerTwoScore;  
+
+        public Game(string server, string receiver)
+        {
+            this._server = server;
+            this._receiver = receiver;
+        }
+
+        public string Score()
+        {
+            var score = "";
+
+            if (_playerOneScore == _playerTwoScore)
+                score = TranslatePointToScore(_playerOneScore) + " All";
+            else
+                score = TranslatePointToScore(_playerOneScore) + " " + TranslatePointToScore(_playerTwoScore);
+            return score;
+        }
+
+        public void PlayerOneWinPoint()
+        {
+            _playerOneScore++;
+        }
+
+        private string TranslatePointToScore(int point)
+        {
+            var score = "";
+            switch (point)
+            {
+                case 1:
+                    score = "Fifteen";
+                    break;
+                case 2:
+                    score = "Thirty";
+                    break;
+                case 3:
+                    score = "Forty";
+                    break;
+                default:
+                    score = "Love";
+                    break;
+            }
+
+            return score;
+        }
+    }
+}
