0232f7817c1448d53afc7f885b0f7fdbed402085
Jed <jed1978@gmail.com>
2018-04-16 22:08:49 +0800

    Add class Game

---
+++ TennisGame/Game.cs
@@ -0,0 +1,14 @@
+﻿namespace TennisGame
+{
+    public class Game
+    {
+        public Game(string v, string v1)
+        {
+        }
+
+        public string Score()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
