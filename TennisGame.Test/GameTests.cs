84ce3f5fcf64dba2d771f895052378a029af0049
Jed <jed1978@gmail.com>
2018-04-16 22:07:40 +0800

    Add test case TestNewGame

---
+++ TennisGame.Test/GameTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssert;
+using NUnit.Framework;
+
+namespace TennisGame.Test
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void TestNewGame()
+        {
+            var target = new Game("Rafael Nadal", "Roger Federer");
+
+            var expected = "0:0";
+
+            target.Score().ShouldBeEqualTo(expected);
+        }
+    }
+}
