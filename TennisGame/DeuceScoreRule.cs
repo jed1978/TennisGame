5d9868cfb47bb68c887476ebd2f9391d74f11b77
Jed Lin <jed1978@gmail.com>
2018-05-26 19:23:42 +0800

    Refactor rules, extract to class

---
+++ TennisGame/DeuceScoreRule.cs
@@ -0,0 +1,33 @@
+﻿namespace TennisGame
+{
+    public class DeuceScoreRule
+    {
+        public string Result { get; private set; }
+
+        public DeuceScoreRule()
+        {
+            Result = "";
+        }
+
+        public string Match(int serverPoint, int receiverPoint)
+        {
+            
+            if (serverPoint == receiverPoint && serverPoint == 3)
+            {
+                Result = "Deuce";
+            }
+
+            if (serverPoint == 4 && receiverPoint == 3)
+            {
+                Result = "Advantage in";
+            }
+
+            if (serverPoint == 3 && receiverPoint == 4)
+            {
+                Result = "Advantage out";
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ TennisGame/RegularScoreRule.cs
@@ -0,0 +1,34 @@
+﻿namespace TennisGame
+{
+    public class RegularScoreRule
+    {
+        public RegularScoreRule()
+        {
+        }
+
+        public string Match(int serverPoint, int receiverPoint)
+        {
+            return $"{DescribeScore(serverPoint)}:{DescribeScore(receiverPoint)}";
+
+        }
+
+        private string DescribeScore(int point)
+        {
+            string score = "Love";
+            if (point == 1)
+            {
+                return "Fifteen";
+            }
+            else if (point == 2)
+            {
+                return "Thirty";
+            }
+            else if (point == 3)
+            {
+                return "Forty";
+            }
+
+            return score;
+        }
+    }
+}
