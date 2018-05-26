18e339a61d3f977aa695a55a5979cb9c4a532f46
Jed Lin <jed1978@gmail.com>
2018-05-26 09:21:20 +0800

    Add test case TwoPlayerJoinTheGame

---
+++ TennisGame.Test/GameTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssert;
+using NUnit.Framework;
+
+namespace TennisGame.Test
+{
+    [TestFixture]
+    public class GameTest
+    {
+        [Test]
+        public void Test_TwoPlayerJoinTheGame()
+        {
+            var game = new Game("Nick Kyrgios", "Dominic Thiem");
+            game.Server.ShouldNotBeNull();
+            game.Receiver.ShouldNotBeNull();
+
+        }
+    }
+
+    public class Game
+    {
+        public Game(string server, string receiver)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Player Server { get; private set; }
+        public Player Receiver { get; private set; }
+
+        public class Player
+        {
+        }
+    }
+}
