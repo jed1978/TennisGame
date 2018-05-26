95b6fe6174bcab6b89076c0893d72185d22f2b63
Jed <jed1978@gmail.com>
2018-05-27 01:20:01 +0800

    Revert "Revert "Refactor, move Player.Point and Player.EarnedPoint() to game""
    This reverts commit d71e441212d058a1c24e8dc0a2443f10cb63dba8.

--- TennisGame/Player.cs
+++ TennisGame/Player.cs
@@ -2,18 +2,11 @@
 {
     public class Player
     {
-        public int Point { get; private set; }
         public string Name { get; }
 
         public Player(string name)
         {
             Name = name;
-            Point = 0;
-        }
-
-        public void EarnedPoint()
-        {
-            Point++;
         }
     }
     
